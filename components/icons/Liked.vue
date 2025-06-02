8040f33ce5de349892a3b292a9341ce602c7d616
Santiago Marchena ÁAlvarez <vansquetch@gmail.com>
2025-06-02 05:18:18 -0500

    =adding Like Icon

--- components/icons/Liked.vue
+++ components/icons/Liked.vue
@@ -10,16 +10,16 @@ const { size = "1.5em" } = defineProps<{
       xmlns="http://www.w3.org/2000/svg"
       :width="size"
       :height="size"
-      viewBox="0 0 24 24"
+      viewBox="0 0 48 48"
     >
+      <!-- Icon from IconPark Solid by ByteDance - https://github.com/bytedance/IconPark/blob/master/LICENSE -->
       <path
         fill="currentColor"
         stroke="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
-        stroke-width="1.5"
-        d="M12 21c-1.341 0-2.114-.572-3.66-1.717C.222 13.275 1.018 6.153 4.537 3.994c2.682-1.645 5.023-.982 6.429.074c.576.433.864.65 1.034.65s.458-.217 1.034-.65c1.406-1.056 3.747-1.719 6.429-.074C21.18 5.048 22.25 7.286 21.949 10"
-        color="currentColor"
+        stroke-width="4"
+        d="M15 8C8.925 8 4 12.925 4 19c0 11 13 21 20 23.326C31 40 44 30 44 19c0-6.075-4.925-11-11-11c-3.72 0-7.01 1.847-9 4.674A10.99 10.99 0 0 0 15 8"
       />
     </svg>
   </span>
